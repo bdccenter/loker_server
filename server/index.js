843d49af9045fa87d0539edba4b223e1b0b72e8a
mcastillo <mcastillo@gruponissauto.com.mx>
2025-04-24 17:55:26 -0700

    second commit

--- server/index.js
+++ server/index.js
@@ -305,6 +305,6 @@ app.post('/login', async (req, res) => {
 });
 
 // Iniciar el servidor
-app.listen(Number(process.env.PORT) || 3000, "0.0.0.0", () => {
+app.listen( port, "0.0.0.0", () => {
   console.log("Flashcardly server is now running!")
 })
